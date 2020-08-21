a9907aea5b9e490196ba775de1913cbb8d9bcc6c
Disteonne <diana.razmikovna@yandex.ru>
2020-08-21 15:01:02 +0300

    Adding new class

---
+++ src/main/java/gitOne/A.java
@@ -0,0 +1,13 @@
+package gitOne;
+
+public class A {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
